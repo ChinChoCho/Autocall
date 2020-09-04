a73a2c85701c2cfc8ada7d48fe0e8eafd5d0df81
Vadim <48584192+ChinChoCho@users.noreply.github.com>
2020-09-04 18:43:54 +0300

    Autocall monitoring complited

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
+
+// Отправка get запроса на сервер по URL
 function get(myUrl) {
 
    let xhr = new XMLHttpRequest();
 
-   xhr.open("GET", myUrl, false);
+   xhr.open("GET", myUrl, false); // false - Синхронный запрос
    xhr.send();
 
-   return xhr.responseText;
+   return xhr.responseText; //строка
 }
 
-//let obj = JSON.parse(get("https://jsonplaceholder.typicode.com/todos"));
-// http:/188.242.196.218:5566/profiles
-
+// Формирование и добавление строк таблицы
 function addRows(obj) {
 
    let element = document.getElementById("active-content");
-   element.innerHTML = "";
+   element.innerHTML = ""; // Очистка таблицы
 
    for (let i = 0; i < obj.length; i++) {
 
+      // Создание новой строки
       let tr = document.createElement('tr');
       tr.className = "table__row";
 
-      tr.innerHTML = "<td class='table__item table__item_content'><span>" + obj[i].userId +
-         "</span></td><td class='table__item table__item_content'><span>" + obj[i].id +
-         "</span></td><td class='table__item table__item_content'><span>" + obj[i].title +
-         "</span></td><td class='table__item table__item_content'><span>" + obj[i].completed +
+      // Формирование ячеек в строке
+      tr.innerHTML = "<td class='table__item table__item_content'><span>" + obj[i].id +
+         "</span></td><td class='table__item table__item_content'><span>" + obj[i].name +
+         "</span></td><td class='table__item table__item_content'><span>" + obj[i].address +
+         "</span></td><td class='table__item table__item_content'><span>" + obj[i].phone +
+         "</span></td><td class='table__item table__item_content'><span>" + obj[i].birthday +
          "</span></td>";
 
-      element.append(tr);
+      element.append(tr); // Добавление строки в таблицу
    }
 }
 
-//addRows();
-
+// Обновление информации на странице
 let timerId = setInterval(function () {
-   let obj = JSON.parse(get("https://jsonplaceholder.typicode.com/todos"));
+   let obj = JSON.parse(get("http://188.242.196.218:5566/profiles")); // Json - файл (объект)
    addRows(obj);
 }, 500);
+
+//https://jsonplaceholder.typicode.com/todos
+//http://188.242.196.218:5566/profiles
